5fb096dbb8bce30b81f0332c9eabef65ffa53e2f
Theo Drackett <37917495+theodrackett@users.noreply.github.com>
2024-01-16 18:22:57 -0800

    Add header and footer

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ import expess from "express"
 
 const app = expess()
 const port = 3000
+const currentYear = new Date().getFullYear()
 
 app.use(expess.static("public"))
 
 app.get("/", (req, res) => {
-    res.render("home.ejs")
+    res.render("home.ejs", {currentYear: currentYear})
 })
 
+app.get("/resume", (req, res) => {
+    res.render("resume.ejs")
+})
+
+app.get("/hobbies", (req, res) => {
+    res.render("hobbies.ejs")
+})
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs")
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
